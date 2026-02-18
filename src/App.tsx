10b871fd32549530b0fd3c56c4962ffa583f6591
andsStosGomes <ands_ngoprt2@hotmail.com>
2026-02-18 14:44:29 -0300

    feat : adicionando Fonts e Splash Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import AppRoutes from '@/Routes';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import * as SplashScreen from 'expo-splash-screen';
+import { 
+         Inter_800ExtraBold, 
+         Inter_400Regular, 
+         Inter_400Regular_Italic, 
+         useFonts } from '@expo-google-fonts/inter';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded] = useFonts({
+    regular: Inter_400Regular,
+    regularItalic: Inter_400Regular_Italic,
+    extraBold: Inter_800ExtraBold
+  }); 
+
+
+  React.useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) return null;
+
+
   return (
     <View style={{ flex: 1 }}>
       <AppRoutes />
